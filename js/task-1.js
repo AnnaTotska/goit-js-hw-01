2d36e7c189525ee5ca01596c2c54ffb126456dbd
Anna <annatotskay69@gmail.com>
2024-12-01 22:11:51 +0100

    finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  const message = `You oredered ${quantity} droids worth ${totalPrice} credits!`;
-  console.log(message);
+  return `You oredered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-  console.log(message);
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
